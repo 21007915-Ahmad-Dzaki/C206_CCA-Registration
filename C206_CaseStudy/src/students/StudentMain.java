dd2fc5631201f9a535866c3137f27951b1461299
21027690 <21027690@LAPTOP-PJD95964>
2022-08-01 00:32:19 +0800

    done view and add student

--- C206_CaseStudy/src/students/StudentMain.java
+++ C206_CaseStudy/src/students/StudentMain.java
@@ -46,19 +46,43 @@ public class StudentMain {
 	}
 	
 	public static void menu() {
-		System.out.println("WELCOME TO STUDENT PROFILES!");
-		Helper.lines(32, "-");
+		System.out.println("WELCOME TO STUDENT PROFILES");
+		Helper.lines(40, "-");
 		System.out.println("1. View All Students");
 		System.out.println("2. Add Student");
 		System.out.println("3. Delete Student");
 		System.out.println("4. Quit");
 	}
 	public static void viewAllStudents(ArrayList<Student> studList) {
+		String output = "";
+		output += String.format("%-10s%-15s%-10s%-10s%-20s\n", "ID", "NAME", "GRADE", "CLASS", "TEACHER");
 		
+		for (int i = 0; i < studList.size(); i++) {
+			output += String.format("%-10d%-15s%-10d%-10s%-20s\n", studList.get(i).getID(), studList.get(i).getName(),
+					studList.get(i).getGrade(), studList.get(i).getStudClass(),studList.get(i).getTeacher());
+		}
+		System.out.println(output);
 	}
 	
 	public static void addStudent(ArrayList<Student> studList) {
+		boolean studAdd = false;
+		int id = Helper.readInt("Enter new student ID > ");
+		String name = Helper.readString("Enter new student name > ");
+		int grade = Helper.readInt("Enter new student grade > ");
+		String studClass = Helper.readString("Enter new student class > ");
+		String teacher = Helper.readString("Enter new student classroom teacher > ");
 		
+		for (int i = 0; i < studList.size();) {
+			studList.add(new Student(id, name, grade, studClass, teacher));
+			studAdd = true;
+			break;
+		}
+		
+		if(studAdd) {
+			System.out.println("New student added!");
+		} else {
+			System.out.println("Failed to add new student!");
+		}
 	}
 	
 	public static void deleteStudent(ArrayList<Student> studList) {
