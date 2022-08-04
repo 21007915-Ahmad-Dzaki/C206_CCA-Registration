b9bee66c45e4bd85648adce5e1d858721a50a135
21010539 <21010539@DESKTOP-SPNKEM1>
2022-08-04 19:48:22 +0800

    Test case for mine

--- C206_CaseStudy/test/TestCat.java
+++ C206_CaseStudy/test/TestCat.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -6,23 +7,22 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import cca_cat.Category;
-import cca_cat.Main;
-
 public class TestCat {
 	
 	private Category c1;
 	private Category c2;
 	private Category c3;
 	
+	private ArrayList<CCA> detail;
 	private ArrayList<Category> obj;
 
 	@Before
 	public void setUp() throws Exception {
-		c1 = new Category("Sports");
-		c2 = new Category("Musics");
-		c3 = new Category("Arts");
+		c1 = new Category("Sports", detail);
+		c2 = new Category("Musics", detail);
+		c3 = new Category("Arts", detail);
 		
+		detail = new ArrayList<CCA>();
 		obj = new ArrayList<Category>();
 	}
 
@@ -32,20 +32,20 @@ public class TestCat {
 		assertNotNull(obj);
 		
 		//Test if list taken from main but its empty (boundary)
-		String methodTest = Main.loadData(obj);
+		String methodTest = C206_CaseStudy.loadDataCat(obj);
 		String empty = "";
 		assertEquals(methodTest, empty);
 		
 		//Test after adding 3 Categories, total object in list is 3 (normal)
-		Main.addCat(obj, c1);
-		Main.addCat(obj, c2);
-		Main.addCat(obj, c3);
+		C206_CaseStudy.addCat(obj, c1);
+		C206_CaseStudy.addCat(obj, c2);
+		C206_CaseStudy.addCat(obj, c3);
 		assertEquals(obj.size(), 3);
 		
 		//Test if output is equal to the data retrieve from list
-		String realOuput = "Categories: " + Main.loadData(obj);
-		String testOuput = "Categories: \nSports\nMusics\nArts";
-		assertEquals(realOuput, testOuput);
+		String realOutput = "Categories: " + C206_CaseStudy.loadDataCat(obj);
+		String testOuput = "Categories: \n- Sports\n- Musics\n- Arts";
+		assertEquals(realOutput, testOuput);
 	}
 
 	@Test
@@ -55,19 +55,19 @@ public class TestCat {
 		
 		//Test after added a Category, the size of the list is 1 (normal) (a)
 		//Test that item added to the list is equals to the first item in the list (b)
-		Main.addCat(obj, c1);	
+		C206_CaseStudy.addCat(obj, c1);	
 		assertEquals(1, obj.size()); //(a)
 		assertSame(c1, obj.get(0)); //(b)
 		
 		//Test if item list is 2 when added another Category (normal) (a)
 		//Test that item added to the list is equals to the second item in the list (b)
-		Main.addCat(obj, c2);
+		C206_CaseStudy.addCat(obj, c2);
 		assertEquals(2, obj.size()); //(a)
 		assertSame(c2, obj.get(1)); //(b)
 		
 		//Test if item list is 3 when added another Category (normal) (a)
 		//Test that item added to the list is equals to the third item in the list (b)
-		Main.addCat(obj, c3);
+		C206_CaseStudy.addCat(obj, c3);
 		assertEquals(3, obj.size()); //(a)
 		assertSame(c3, obj.get(2)); //(b)
 	}
@@ -75,17 +75,17 @@ public class TestCat {
 	@Test
 	public void testRemoveCat() {
 		//Test list is not empty so that it can remove categories (boundary)
-		Main.addCat(obj, c1);
-		Main.addCat(obj, c2);
-		Main.addCat(obj, c3);
+		C206_CaseStudy.addCat(obj, c1);
+		C206_CaseStudy.addCat(obj, c2);
+		C206_CaseStudy.addCat(obj, c3);
 		assertTrue(!obj.isEmpty());
 		
-		//Test if item list is 2 when a Category is removed (normal)
-		Main.removeCat(obj);
+		//Test if item list is 2 when a Category is removed (normal) (use: Sports)
+		C206_CaseStudy.removeCat(obj);
 		assertEquals(2, obj.size());
 		
-		//Test if Category list remains 2 when removing a Category that does not exist 
-		Main.removeCat(obj);
+		//Test if Category list remains 2 when removing a Category that does not exist (Error) (use: Sports)
+		C206_CaseStudy.removeCat(obj);
 		assertEquals(2, obj.size());
 	}
 	
@@ -95,6 +95,7 @@ public class TestCat {
 		c2 =  null;
 		c3 =  null;
 		
+		detail = null;
 		obj = null;
 	}
 
